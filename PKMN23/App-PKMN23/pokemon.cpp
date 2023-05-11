c7d82160c86e8b65ad9af1852793066065845866
Thibaut Lefrancois <thibaut.lefrancois@etudiant.univ-lr.fr>
2023-05-11 21:15:09 +0200

    Fin de version 0.1

--- PKMN23/App-PKMN23/pokemon.cpp
+++ PKMN23/App-PKMN23/pokemon.cpp
@@ -26,5 +26,13 @@ void Pokemon::setItsHP(int newItsHP)
 
 void Pokemon::attack(Pokemon * anOpponent)
 {
+    //Actualiser le nombre de points de vie de l'ennemie selon le nombre de dégats que notre pokémon peut lui faire
     anOpponent->setItsHP(anOpponent->getItsCurrentHP() - this->nbDamage(anOpponent));
+
+    //Si il est mort, s'assurer que sa vie est à 0 et redonner la vie au pokémon victorieux
+    if(anOpponent->itsCurrentHP <= 0)
+    {
+        anOpponent->setItsHP(0);
+        this->setItsHP(this->itsMaxHP);
+    }
 }
